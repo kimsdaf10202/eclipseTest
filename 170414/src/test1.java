f7a49b814546517315e293753202cfeb69c3f926
user <user@edi05>
2017-04-14 14:45:09 +0900

    김민성 김준석으로 수정

--- 170414/src/test1.java
+++ 170414/src/test1.java
@@ -5,7 +5,8 @@ public class test1 {
 		System.out.println("°íÇö¼­");
 		System.out.println("±è°æ¿î");
 		System.out.println("±èµ¿Çö");
-		System.out.println("±è¹Î¼º");
+		System.out.println("±èÁØ¼®");
+		System.out.println("±è¹ÎÁØ");
 
 	}
 
